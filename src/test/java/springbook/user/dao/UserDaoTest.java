31ea6e484a926fd9eeac6b1469310899c2d89e8d
sehun-kim <rogarithm@gmail.com>
2022-10-04 14:01:20 +0900

    2.3.2 테스트 전 준비를 자동화하기 위한 메소ㅓ드 및 검증
     - 책에서는 addAndGet에서만 새로 만든 메소드를 검증하지만 나는 따로
     테스트할 테스트 메소드도 추가로 만들었다.

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,23 +1,40 @@
 package springbook.user.dao;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.hamcrest.Matchers.is;
 
 import java.sql.SQLException;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
 public class UserDaoTest {
 
-    public static void main(String[] args) {
-    }
     @Test
     public void addAndGet() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
+
+        User user = new User();
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
+
+        dao.add(user);
+        assertThat(dao.getCount()).isEqualTo(1);
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName()).isEqualTo(user.getName());
+        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+    }
+
+    @Test
+    public void countDiffAfterDelete() throws SQLException {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
@@ -29,9 +46,10 @@ public class UserDaoTest {
 
         dao.add(user);
 
-        User user2 = dao.get(user.getId());
-
-        assertThat(user2.getName()).isEqualTo(user.getName());
-        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+        int countBefore = dao.getCount();
+        dao.deleteAll();
+        int countAfter = dao.getCount();
+        assertThat(countBefore).isEqualTo(1);
+        assertThat(countAfter).isEqualTo(0);
     }
 }

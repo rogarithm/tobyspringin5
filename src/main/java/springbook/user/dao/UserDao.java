0d6fcc5e56bad324452f8be70cbba685250da9d2
sehun-kim <rogarithm@gmail.com>
2022-10-07 00:55:25 +0900

    3.2.2 클라이언트가 전략을 정해서 전달하도록 함

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -58,15 +58,14 @@ public class UserDao {
         return user;
     }
 
-    public void deleteAll() throws SQLException {
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
         Connection c = null;
         PreparedStatement ps = null;
 
         try {
             c = dataSource.getConnection();
 
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
+            ps = stmt.makePreparedStatement(c);
 
             ps.executeUpdate();
         } catch (SQLException e) {
@@ -85,6 +84,11 @@ public class UserDao {
                 }
             }
         }
+    }
+
+    public void deleteAll() throws SQLException {
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount() throws SQLException {

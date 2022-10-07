d2063e63d647fafb142ff4fb0d0937079704e6f6
sehun-kim <rogarithm@gmail.com>
2022-10-07 18:01:00 +0900

    3.3.2 전략 클래스를 메소드 내 로컬 클래스로 이동

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -16,7 +16,26 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws SQLException {
+    public void add(final User user) throws SQLException {
+        class AddStatement implements StatementStrategy {
+
+            User user;
+
+            public AddStatement(User user) {
+                this.user = user;
+            }
+
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement(
+                        "insert into users(id, name, password) values(?,?,?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+
+                return ps;
+            }
+        }
+
         AddStatement st = new AddStatement(user);
         jdbcContextWithStatementStrategy(st);
     }

ea531421ee3520d0c1da4285925fcf0e11f61457
sehun-kim <rogarithm@gmail.com>
2022-10-08 17:02:15 +0900

    3.5.3 calcSumTest 초기화 메소드 분리

--- src/test/java/springbook/learningtest/template/CalcSumTest.java
+++ src/test/java/springbook/learningtest/template/CalcSumTest.java
@@ -2,14 +2,22 @@ package springbook.learningtest.template;
 
 import java.io.IOException;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalcSumTest {
 
+    private Calculator calculator;
+    private String numFilepath;
+
+    @BeforeEach
+    public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilepath = getClass().getResource("numbers.txt").getPath();
+    }
+
     @Test
     public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
-        Assertions.assertThat(sum).isEqualTo(10);
+        Assertions.assertThat(calculator.calcSum(this.numFilepath)).isEqualTo(10);
     }
 }

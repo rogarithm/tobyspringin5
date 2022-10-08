ba6f62b2d3015541b244c13e40452938ecbae1cf
sehun-kim <rogarithm@gmail.com>
2022-10-08 16:30:02 +0900

    3.5 lambda와 try-with-resources 적용

--- src/main/java/springbook/user/dao/JdbcContext.java
+++ src/main/java/springbook/user/dao/JdbcContext.java
@@ -15,43 +15,18 @@ public class JdbcContext {
 
     public void executeSql(final String query) throws SQLException {
         workWithStatementStrategy(
-                new StatementStrategy() {
-                    @Override
-                    public PreparedStatement makePreparedStatement(Connection c)
-                            throws SQLException {
-                        PreparedStatement ps = c.prepareStatement(query);
-                        return ps;
-                    }
-                }
+                c -> c.prepareStatement(query)
         );
     }
 
 
     public void workWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = this.dataSource.getConnection();
-
-            ps = stmt.makePreparedStatement(c);
-
+        try (Connection c = this.dataSource.getConnection();
+                PreparedStatement ps = stmt.makePreparedStatement(c);
+        ) {
             ps.executeUpdate();
         } catch (SQLException e) {
             throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
         }
     }
 }
--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -21,19 +21,15 @@ public class UserDao {
     private JdbcContext jdbcContext;
 
     public void add(final User user) throws SQLException {
-
         this.jdbcContext.workWithStatementStrategy(
-                new StatementStrategy() {
-                    public PreparedStatement makePreparedStatement(Connection c)
-                            throws SQLException {
-                        PreparedStatement ps = c.prepareStatement(
-                                "insert into users(id, name, password) values(?,?,?)");
-                        ps.setString(1, user.getId());
-                        ps.setString(2, user.getName());
-                        ps.setString(3, user.getPassword());
+                c -> {
+                    PreparedStatement ps = c.prepareStatement(
+                            "insert into users(id, name, password) values(?,?,?)");
+                    ps.setString(1, user.getId());
+                    ps.setString(2, user.getName());
+                    ps.setString(3, user.getPassword());
 
-                        return ps;
-                    }
+                    return ps;
                 }
         );
     }

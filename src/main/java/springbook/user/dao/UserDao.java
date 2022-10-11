226701e5f9686faa9e35f8f7de9edbec0140e99e
sehun-kim <rogarithm@gmail.com>
2022-10-11 21:41:33 +0900

    3.6.3 getCount() try-with-resources로 변경

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -66,39 +66,13 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            c = dataSource.getConnection();
-
-            ps = c.prepareStatement("select count(*) from users");
-
-            rs = ps.executeQuery();
+        try (Connection c = dataSource.getConnection();
+                PreparedStatement ps = c.prepareStatement("select count(*) from users");
+                ResultSet rs = ps.executeQuery();) {
             rs.next();
             return rs.getInt(1);
         } catch (SQLException e) {
             throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
         }
     }
 }

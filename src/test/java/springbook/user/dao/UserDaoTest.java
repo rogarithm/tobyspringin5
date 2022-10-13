de9445787608eacaac689480a35066ed1d6f2657
sehun-kim <rogarithm@gmail.com>
2022-10-13 19:13:10 +0900

    3.6.4 getAll() 데이터 없는 경우 테스트 추가

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -84,6 +84,9 @@ public class UserDaoTest {
     public void getAll() throws SQLException {
         dao.deleteAll();
 
+        List<User> users0 = dao.getAll();
+        assertThat(users0.size()).isEqualTo(0);
+
         dao.add(user1);
         List<User> users1 = dao.getAll();
         assertThat(users1.size()).isEqualTo(1);

d663cf8b9dcf6a65bcb182af80fbcfa50c38567c
sehun-kim <rogarithm@gmail.com>
2022-10-17 22:53:03 +0900

    users 테이블 생성 sql 파일 수정
     - 터미널로 로컬 db 접속해서 새로 생긴 필드 추가했다

--- sql/users_create.sql
+++ sql/users_create.sql
@@ -1,5 +1,11 @@
-create table users (
+create table if not exists users (
 	id varchar(10) primary key,	
 	name varchar(20) not null,
 	password varchar(10) not null
-)
+);
+alter table users
+add column level tinyint not null after password;
+alter table users
+add column login int not null after level;
+alter table users
+add column recommend int not null after login;

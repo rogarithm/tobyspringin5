e45a2427edbb2c0b66140626a4871e9a01b7f43e
sehun-kim <rogarithm@gmail.com>
2022-10-17 23:15:50 +0900

    5.1.2 사용자 수정 기능 테스트 추가

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -137,4 +137,21 @@ public class UserDaoTest {
             assertThat(set.translate(null, null, sqlEx)).isInstanceOf(DuplicateKeyException.class);
         }
     }
+
+    @Test
+    public void update() {
+        dao.deleteAll();
+
+        dao.add(user1);
+
+        user1.setName("오민규");
+        user1.setPassword("springno6");
+        user1.setLevel(Level.GOLD);
+        user1.setLogin(1000);
+        user1.setRecommend(999);
+        dao.update(user1);
+
+        User user1update = dao.get(user1.getId());
+        checkSameUser(user1, user1update);
+    }
 }

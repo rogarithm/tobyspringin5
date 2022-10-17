eaf1052bb1d68d3c375296c0f7e3c14c7c60967f
sehun-kim <rogarithm@gmail.com>
2022-10-17 22:52:16 +0900

    5.1.1 Level 이늄 추가에 따른 User 필드 추가

--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -8,7 +8,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+
     private String id;
     private String name;
     private String password;
+    private Level level;
+    private int login;
+    private int recommend;
 }

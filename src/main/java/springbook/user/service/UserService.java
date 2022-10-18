5cd1811a3e5c0e0899aa78e6b3a65c7164af476d
sehun-kim <rogarithm@gmail.com>
2022-10-18 23:23:41 +0900

    5.1.3 UserService에 회원 레벨 업그레이드 메소드 추가

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -1,6 +1,9 @@
 package springbook.user.service;
 
+import java.util.List;
 import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
 
 public class UserService {
 
@@ -8,5 +11,26 @@ public class UserService {
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            Boolean changed = null;
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            } else if (user.getLevel() == Level.GOLD) {
+                changed = false;
+            } else {
+                changed = false;
+            }
+            if (changed) {
+                userDao.update(user);
+            }
+        }
     }
 }

1f663c52f2612388bd7f81612a2eb973e92d3c6f
sehun-kim <rogarithm@gmail.com>
2022-10-21 00:46:41 +0900

    5.1.5 User 테스트 추가

---
+++ src/test/java/springbook/user/service/UserTest.java
@@ -0,0 +1,45 @@
+package springbook.user.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+public class UserTest {
+
+    User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User();
+    }
+
+    @Test
+    public void upgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() == null) {
+                continue;
+            }
+            user.setLevel(level);
+            user.upgradeLevel();
+            Assertions.assertThat(user.getLevel()).isEqualTo(level.nextLevel());
+        }
+    }
+
+    @Test
+    public void cannotUpgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() != null) {
+                continue;
+            }
+            user.setLevel(level);
+            assertThrows(IllegalStateException.class, () -> user.upgradeLevel());
+        }
+    }
+
+}

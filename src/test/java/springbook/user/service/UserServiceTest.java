a1d33aa666523d4ac9e37d29de413214be970d71
sehun-kim <rogarithm@gmail.com>
2022-10-21 23:22:40 +0900

    5.2.1 테스트용 UserService 대역 생성
     - 트랜잭션 테스트를 위해 upgradeLevel 메소드를 상속받아 그 동작을
       UserService 대역에서 바꿨다.
     - 테스트용 예외를 만들었다.

--- src/test/java/springbook/user/service/UserServiceTest.java
+++ src/test/java/springbook/user/service/UserServiceTest.java
@@ -83,4 +83,24 @@ public class UserServiceTest {
         Assertions.assertThat(userWithLevelRead.getLevel()).isEqualTo(userWithLevel.getLevel());
         Assertions.assertThat(userWithoutLevelRead.getLevel()).isEqualTo(Level.BASIC);
     }
+
+    static class TestUserService extends UserService {
+
+        private String id;
+
+        private TestUserService(String id) {
+            this.id = id;
+        }
+
+        @Override
+        protected void upgradeLevel(User user) {
+            if (user.getId().equals(this.id))
+                throw new TestUserServiceException();
+            super.upgradeLevel(user);
+        }
+    }
+
+    static class TestUserServiceException extends RuntimeException {
+
+    }
 }

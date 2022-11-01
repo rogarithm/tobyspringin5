3bed1f22ce2de4a5a625111a5c812a2a51f8ba91
sehun-kim <rogarithm@gmail.com>
2022-11-01 22:18:20 +0900

    다이내믹 프록시 적용

---
+++ src/test/java/springbook/learningtest/proxy/HelloTest.java
@@ -0,0 +1,33 @@
+package springbook.learningtest.proxy;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.lang.reflect.Proxy;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+    @Test
+    public void simpleProxy() {
+        Hello hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
+        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
+    }
+
+    @Test
+    public void helloUppercaseTest() {
+        Hello proxiedHello = new HelloUppercase(new HelloTarget());
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    @Test
+    public void dynamicProxyTest() {
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{Hello.class},
+                new UppercaseHandler(new HelloTarget()));
+    }
+}

522378ca82d443a103e9c1a1414cb8604b36cf6f
sehun-kim <rogarithm@gmail.com>
2022-11-02 14:39:41 +0900

    6.3.3 upgradeAllOrNothing 테스트를 다이내믹 프록시 방식으로 변경

--- src/test/java/springbook/user/service/UserServiceTest.java
+++ src/test/java/springbook/user/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.when;
 import static springbook.user.service.UserServiceImpl.MIN_LOGCOUNT_FOR_SILVER;
 import static springbook.user.service.UserServiceImpl.MIN_RECCOMEND_FOR_GOLD;
 
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -153,9 +154,13 @@ public class UserServiceTest {
         testUserService.setUserDao(this.userDao);
         testUserService.setMailSender(mailSender);
 
-        UserServiceTx txUserService = new UserServiceTx();
-        txUserService.setTransactionManager(transactionManager);
-        txUserService.setUserService(testUserService);
+        TransactionHandler txHandler = new TransactionHandler();
+        txHandler.setTarget(testUserService);
+        txHandler.setTransactionManager(transactionManager);
+        txHandler.setPattern("upgradeLevels");
+        UserService txUserService = (UserService) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{UserService.class}, txHandler);
 
         userDao.deleteAll();
         for (User user : users) {

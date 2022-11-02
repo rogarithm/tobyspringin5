b5d443c0e0ca6cc1713d84407c6f312c9a6cb8b7
sehun-kim <rogarithm@gmail.com>
2022-11-02 11:27:16 +0900

    6.1.1 비즈니스 로직과 트랜잭션 경계설정 분리

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -46,16 +46,20 @@ public class UserService {
         TransactionStatus status = this.transactionManager.getTransaction(
                 new DefaultTransactionDefinition());
         try {
-            List<User> users = userDao.getAll();
-            for (User user : users) {
-                if (canUpgradeLevel(user)) {
-                    upgradeLevel(user);
-                }
-            }
+            upgradeLevelsInternal();
             this.transactionManager.commit(status);
         } catch (RuntimeException e) {
             this.transactionManager.rollback(status);
             throw e;
+        }
+    }
+
+    private void upgradeLevelsInternal() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
+            }
         }
     }
 
